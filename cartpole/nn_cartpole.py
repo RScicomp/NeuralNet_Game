ef521525473ace412fee0c2cc0c026733c4f0158
Brian Cheng <chengbrian7@gmail.com>
2020-05-21 09:54:59 -0400

    properly set up the game, using random actions still

--- cartpole/nn_cartpole.py
+++ cartpole/nn_cartpole.py
@@ -11,19 +11,38 @@ classifier.add(Dense(input_dim=4, output_dim=6, init='uniform', activation='sigm
 classifier.add(Dense(output_dim=1, init='uniform', activation='sigmoid'))
 classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
+
+
 # -------------------------------------------------------------------------------------
 # agent:
 class Agent():
+
+
     def __init__(self,env):
         self.action_size = env.action_space.n
+        self.x_train = np.array([])
+        self.y_train = np.array([])
 
     def get_action(self, state):
-        cart_pos = state[0]
-        cart_vel = state[1]
-        pole_ang = state[2]
-        pole_vel = state[3]
-        return 0
+        cart_pos = state[0]/10
+        cart_vel = state[1]/100
+        pole_ang = state[2]/100
+        pole_vel = state[3]/100
 
+        inputs = np.array([cart_pos, cart_vel, pole_ang, pole_vel])
+        inputs = inputs.flatten()
+        print(inputs.shape)
+        if cart_pos > 0:
+            self.x_train = np.append(self.x_train, inputs)
+            self.y_train = np.append(self.y_train, [0])
+        elif cart_pos < 0:
+            self.x_train = np.append(self.x_train, inputs)
+            self.y_train = np.append(self.y_train, [1])
+
+        return classifier.predict_classes(np.array(state))
+
+    def train(self):
+        classifier.fit(self.x_train,self.y_train)
 
 # -------------------------------------------------------------------------------------
 # game:
@@ -32,9 +51,19 @@ import gym
 env_name = "CartPole-v1"
 env = gym.make(env_name)
 state = env.reset()
+reward = 0
 
 agent = Agent(env)
-for _ in range(200):
-    action = agent.get_action(state)
-    state, reward, done, info = env.step(action)
-    env.render()
+
+def random_games():
+    for episode in range(10):
+        env.reset()
+        for _ in range(200):
+            #action = agent.get_action(state)
+            env.render()
+            action = env.action_space.sample()
+            state, reward, done, info = env.step(action)
+            if done:
+                break
+
+random_games()
