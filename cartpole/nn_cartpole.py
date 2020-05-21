8c12f1a2066cc23a40f76aca745670d14a82aaad
Brian Cheng <chengbrian7@gmail.com>
2020-05-21 18:29:11 -0400

    shape error on output layer

--- cartpole/nn_cartpole.py
+++ cartpole/nn_cartpole.py
@@ -3,62 +3,56 @@
 import keras
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Flatten
 from keras.optimizers import Adam
 
 classifier = Sequential()
-classifier.add(Dense(input_dim=4, output_dim=6, init='uniform', activation='sigmoid'))
+# 3 hidden layers
+classifier.add(Flatten(input_shape=(4, 1)))
+classifier.add(Dense(128, init='uniform', activation='relu'))
+classifier.add(Dense(128, init='uniform', activation='relu'))
+classifier.add(Dense(128, init='uniform', activation='relu'))
+# output layer
 classifier.add(Dense(output_dim=1, init='uniform', activation='sigmoid'))
 classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
 
 
 # -------------------------------------------------------------------------------------
 # agent:
 class Agent():
-
-
-    def __init__(self,env):
+    def __init__(self, env):
         self.action_size = env.action_space.n
         self.x_train = np.array([])
         self.y_train = np.array([])
 
     def get_action(self, state):
-        cart_pos = state[0]/10
-        cart_vel = state[1]/100
-        pole_ang = state[2]/100
-        pole_vel = state[3]/100
-
-        inputs = np.array([cart_pos, cart_vel, pole_ang, pole_vel])
+        inputs = np.array()
         inputs = inputs.flatten()
-        print(inputs.shape)
-        if cart_pos > 0:
-            self.x_train = np.append(self.x_train, inputs)
-            self.y_train = np.append(self.y_train, [0])
-        elif cart_pos < 0:
-            self.x_train = np.append(self.x_train, inputs)
-            self.y_train = np.append(self.y_train, [1])
+        classifier.predict_classes(np.array(state))
 
-        return classifier.predict_classes(np.array(state))
+    def train(self, data):
+        x = np.array([i[0] for i in data]).reshape(-1, len(data[0][0]), 1)
+        print(x.shape)
+        y = np.array(i[1] for i in data)
+        classifier.fit(x, y, epochs=10, verbose=1)
 
-    def train(self):
-        classifier.fit(self.x_train,self.y_train)
 
 # -------------------------------------------------------------------------------------
 # game:
 import gym
 
+
 env_name = "CartPole-v1"
 env = gym.make(env_name)
 state = env.reset()
-reward = 0
 
 agent = Agent(env)
 
+
 def random_games():
-    for episode in range(10):
+    for episode in range(1):
         env.reset()
-        for _ in range(200):
+        for _ in range(500):
             #action = agent.get_action(state)
             env.render()
             action = env.action_space.sample()
@@ -66,4 +60,42 @@ def random_games():
             if done:
                 break
 
-random_games()
+
+def gen_one():
+    score = np.array([])
+    data = []
+
+    for episode in range(500):
+        env.reset()
+
+        step_reward = 0
+        game_data = []
+        prev_obs = []
+
+        for _ in range(500):
+            action = env.action_space.sample()
+            state, reward, done, info = env.step(action)
+
+            if len(prev_obs) > 0:
+                game_data.append([prev_obs, action])
+            prev_obs = state
+            step_reward += reward
+            if done:
+                break
+
+        if step_reward >= 50:
+            score = np.append(score, step_reward)
+            for d in game_data:
+                data.append(d)
+
+    print(score)
+
+    return data
+
+
+gen_one_data = gen_one()
+
+#agent.train(gen_one_data)
+
+# print(gen_one_data.shape)
+
